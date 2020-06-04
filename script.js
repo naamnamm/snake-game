3fccc9db72674c51804bd327e544ba263eb6e4b5
Naam <naam.pt@gmail.com>
2020-06-04 07:07:37 -0400

    make a snake move and turn

--- script.js
+++ script.js
@@ -1,108 +1,88 @@
 //Done
 //draw a snack
 //make snack move
+//control the snack movement
 
 //TO DO
-//control the snack movement
 //draw apple 
 //show apple randomly
 
 const log = console.log
+let interval = null;
+let keyCode = null;
+let canvas = document.getElementById('game-canvas')
+let context = canvas.getContext('2d')
+canvas.width = 600;
+canvas.height = 400;
 
-let xSpeed = 5;
-let ySpeed = 5;
-let intervalX;
-let intervalY;
-let keyCode;
+const snake = {
+  width: 20,
+  height: 20,
+  xSpeed: 10,
+  ySpeed: 0
+}
 
-const stopButtonEl = document.getElementById('stop-btn');
-stopButtonEl.addEventListener('click', stopIntervalX)
-
-// const getKey = document.getElementById('get-key')
-// getKey.addEventListener('keydown', function (e) {
-//   log(e)
-// })
-
+let snakePosition = [
+  { x: 160, y: 150 },
+  { x: 140, y: 150 },
+  { x: 120, y: 150 },
+  { x: 100, y: 150 },
+  { x: 80, y: 150 }
+];
 
 
 window.onload = function () {
-  let canvas = document.getElementById('game-canvas')
-  let context = canvas.getContext('2d')
-  canvas.width = 600;
-  canvas.height = 400;
-  let framePerSecond = 30;
-  let xPosition = 450;
-  let yPosition = 100;
-  let snakeWidth = 20;
 
+  let framePerSecond = 15;
+  interval = setInterval(function () {
+    drawCanvas();
+    moveSnake();
+    drawSnake();
+    controlSnake();
+  }, 1000)
 
-  intervalX = setInterval(function () {
-    xPosition += xSpeed
-    moveSnake(context, xPosition, yPosition, snakeWidth);
-    drawSnake(context, xPosition, yPosition, snakeWidth);
-  }, 1000 / framePerSecond)
+  const stopButtonEl = document.getElementById('stop-btn');
+  stopButtonEl.addEventListener('click', stopInterval)
 
-  intervalY = setInterval(function () {
-    yPosition += ySpeed
-    moveSnake(context, xPosition, yPosition, snakeWidth);
-    drawSnake(context, xPosition, yPosition, snakeWidth);
-  }, 1000 / framePerSecond)
+  document.body.addEventListener('keydown', e => keyCode = e.key)
 
 }
 
-// function startIntervalY() {
-//   setInterval(function () {
-//     let canvas = document.getElementById('game-canvas')
-//     let context = canvas.getContext('2d')
-//     canvas.width = 600;
-//     canvas.height = 400;
-//     let yPosition = 100;
-//     let snakeWidth = 20;
-//     let xPosition = context.canvas.width - snakeWidth
-//     yPosition += ySpeed
-//     moveSnake(context, xPosition, yPosition, snakeWidth);
-//     drawSnake(context, xPosition, yPosition, snakeWidth);
-//   }, 3000)
-// }
-
-
-function stopIntervalX() {
-  clearInterval(intervalX);
+function stopInterval() {
+  clearInterval(interval);
 }
 
-function stopIntervalY() {
-  clearInterval(intervalY);
-}
-
-
-function moveSnake(context, xPosition, yPosition, snakeWidth, keyCode) {
-  //debugger;
-  if (xPosition < (context.canvas.width - snakeWidth)) {
-    stopIntervalY();
-  }
-
-  if (xPosition === (context.canvas.width - snakeWidth)) {
-    stopIntervalX();
-  }
-
-  if (xPosition < 0) {
-    xSpeed = -xSpeed
-    //TO DO: gameover
-  }
-
-}
-
-function drawSnake(context, xPosition, yPosition, snakeWidth, keyCode) {
-  //debugger;
+function drawCanvas() {
   context.fillStyle = 'black';
   context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-
-  context.fillStyle = 'skyblue';
-  context.fillRect(xPosition, yPosition, snakeWidth, 20);
-
-  log(xPosition, yPosition)
-
 }
+
+function drawSnake() {
+  log(snakePosition);
+  snakePosition.forEach(position => drawSnakeParts(position))
+}
+
+function drawSnakeParts(position) {
+  context.fillStyle = 'skyblue';
+  context.fillRect(position.x, position.y, snake.width, snake.height);
+}
+
+function moveSnake() {
+  let head = { x: snakePosition[0].x += snake.xSpeed, y: snakePosition[0].y += snake.ySpeed }
+
+  snakePosition.unshift(head);
+  snakePosition.pop();
+}
+
+function controlSnake() {
+  if (keyCode === 'ArrowDown') {
+    snake.xSpeed = 0;
+    snake.ySpeed = 10;
+  }
+}
+
+
+
 
 
 
